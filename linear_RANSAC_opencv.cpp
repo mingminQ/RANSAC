97864c3b8f376440df145407674a7a173b0d8b1f
mingminQ <149143017+mingminQ@users.noreply.github.com>
2024-08-24 02:02:58 +0900

    Update linear_RANSAC_opencv.cpp

--- linear_RANSAC_opencv.cpp
+++ linear_RANSAC_opencv.cpp
@@ -1,22 +1,22 @@
 #include <vector>
 
-
-cv::Mat linear_RANSAC(const std::vector<cv::Point2f>& points, int iterations, double distance_threshold, int samples)
+cv::Mat linear_RANSAC(const std::vector<cv::Point2f>& points, int iteration, double inlier_distance, int sample_num)
 {
-    // valiable for best model
-    cv::Mat best_model;
-    int best_inlier_count = 0;
+    // best function varibale   
+    cv::Mat bestModel;
+    int bestInliers = 0;
     
     // random sampling
     std::mt19937 rng(std::random_device{}());
-    for (int rept = 0; rept < iterations; rept++)
+    std::uniform_int_distribution<int> dist(0, points.size() - 1);
+    
+    // linear RANSAC fitting
+    for (int repeat = 0; repeat < iteration; repeat++)
     {
-        // select 2 random points
-        std::uniform_int_distribution<int> dist(0, points.size() - 1);
-        int idx1 = dist(rng); cv::Point2f point1 = points[idx1];
-        int idx2 = dist(rng); cv::Point2f point2 = points[idx2];
+        int sample_idx1 = dist(rng); cv::Point2f point1 = points[sample_idx1];
+        int sample_idx2 = dist(rng); cv::Point2f point2 = points[sample_idx2];
 
-        // linear function : ax + by + c = 0
+        // calculate linear function : ax + by + c = 0
         double a = point2.y - point1.y;
         double b = point1.x - point2.x;
         double c = point2.x * point1.y - point1.x * point2.y;
@@ -26,13 +26,13 @@ cv::Mat linear_RANSAC(const std::vector<cv::Point2f>& points, int iterations, do
         for (const auto& point : points)
         {
             double distance = std::abs(a * point.x + b * point.y + c) / std::sqrt(a * a + b * b);
-            if (distance < distance_threshold) inlier_count++;
+            if (distance < inlier_distance) inlier_count++;
         }
 
         // select the model that has the best inliers
-        if (best_inlier_count < inlier_count)
+        if (bestInliers < inlier_count)
         {
-            best_inlier_count = inlier_count;
+            bestInliers = inlier_count;
             best_model = (cv::Mat_<float>(1, 3) << a, b, c);
         }
     }
