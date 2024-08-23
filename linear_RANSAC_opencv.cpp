729904ba9c60564df0686d2b0d0cb7a9bfceb28d
mingminQ <149143017+mingminQ@users.noreply.github.com>
2024-08-24 01:56:30 +0900

    Create linear_RANSAC_opencv.cpp

---
+++ linear_RANSAC_opencv.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+
+
+cv::Mat linear_RANSAC(const std::vector<cv::Point2f>& points, int iterations, double distance_threshold, int samples)
+{
+    // valiable for best model
+    cv::Mat best_model;
+    int best_inlier_count = 0;
+    
+    // random sampling
+    std::mt19937 rng(std::random_device{}());
+    for (int rept = 0; rept < iterations; rept++)
+    {
+        // select 2 random points
+        std::uniform_int_distribution<int> dist(0, points.size() - 1);
+        int idx1 = dist(rng); cv::Point2f point1 = points[idx1];
+        int idx2 = dist(rng); cv::Point2f point2 = points[idx2];
+
+        // linear function : ax + by + c = 0
+        double a = point2.y - point1.y;
+        double b = point1.x - point2.x;
+        double c = point2.x * point1.y - point1.x * point2.y;
+
+        // calculate inliers based on distance
+        int inlier_count = 0;
+        for (const auto& point : points)
+        {
+            double distance = std::abs(a * point.x + b * point.y + c) / std::sqrt(a * a + b * b);
+            if (distance < distance_threshold) inlier_count++;
+        }
+
+        // select the model that has the best inliers
+        if (best_inlier_count < inlier_count)
+        {
+            best_inlier_count = inlier_count;
+            best_model = (cv::Mat_<float>(1, 3) << a, b, c);
+        }
+    }
+
+    // no model found
+    if (best_model.empty()) return cv::Mat();
+
+    // transform ax + by + c = 0 -> y = a'x + b'
+    float a = best_model.at<float>(0, 0);
+    float b = best_model.at<float>(0, 1);
+    float c = best_model.at<float>(0, 2);
+
+    // return slope and intercept
+    float slope     = -a / b;
+    float intercept = -c / b;
+
+    return (cv::Mat_<float>(1, 2) << slope, intercept);
+}
